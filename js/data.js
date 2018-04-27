195bbaac2a25c1be6b98b48c5b03e679a6c19ca7
Alex Brown <abrown@qz.com>
2018-04-27 13:42:58 -0400

    copied over tmeplate, added sawyers text & styled mobile

---
+++ js/data.js
@@ -0,0 +1,26 @@
+var data = {
+    title: 'Super Hero Story',
+    description: 'You\'re a super hero with super speed. You find out a villain is in town square.',
+    question: 'What do you do?',
+    choices: [
+        {
+            answer: 'Kill him with a rock',
+            description: 'The cops arrest you & you go to jail for killing the villain.',
+            question: 'What do you do?',
+            choices: [
+                {
+                    answer: 'Run away',
+                    description: 'The cops find you, put an electrocuter on you, & you die.',
+                },
+                {
+                    answer: 'Stay & live out your sentence',
+                    description: 'You find a good friend in prison & die happy.'
+                }
+            ]
+        },
+        {
+            answer: 'Put handcuffs on him',
+            description: 'You arrest him & everybody praises your name & you die happy.',
+        }
+    ]
+}
